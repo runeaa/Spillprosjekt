3f72fafbb439f69e021c20d463f7233a528200d9
Rune <Rune@Voyager>
2013-08-27 15:02:17 +0200

    Initialiser

---
+++ src/Game/Game.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Game;
+
+/**
+ *
+ * @author Rune
+ */
+public class Game {
+    public static void main(String[] args) {
+        
+    }
+}
