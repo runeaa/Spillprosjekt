3512a2d549c480967d4c318caeabf33e3c4898c7
Rune <Rune@Voyager>
2013-08-27 15:32:33 +0200

    Vindu

--- src/Main/GamePanel.java
+++ src/Main/GamePanel.java
@@ -15,12 +15,40 @@ public class GamePanel extends JPanel{
     
     private static int WIDTH = 240;
     private static int HEIGHT = 240;
+    private boolean running;
+    private Thread thread;
 
     public GamePanel() {
         super();
         setPreferredSize(new Dimension(WIDTH, HEIGHT));
     }
     
+    public void addNotify(){
+        super.addNotify();
+        if(thread == null){
+            thread = new Thread();
+            running = true;
+        }
+    }
     
+    public void init(){}
+    
+    public void run(){
+        init();
+        while(running){
+           //gameloop
+            update();
+            render();
+            draw();
+        }
+    }
+    
+    public void update(){}
+    
+    public void render(){
+        
+    }
+    
+    public void draw(){}
     
 }
