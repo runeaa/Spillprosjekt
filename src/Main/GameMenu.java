67c4d5a0f2be0016b3983b15ef80aaec84b9183d
haavamoa <havard-moas@live.com>
2013-08-29 12:09:52 +0200

    Added GameMenu to work with tomorrow ;)

---
+++ src/Main/GameMenu.java
@@ -0,0 +1,8 @@
+package Main;
+/**
+ *
+ * @author haavamoa
+ */
+public class GameMenu {
+
+}
--- src/Settings/Settings.java
+++ src/Settings/Settings.java
@@ -9,5 +9,21 @@ package Settings;
  * @author Rune
  */
 public class Settings {
+    public int WITDH = 640;
+    public int HEIGHT = 420;
+    public int FPS = 30;
+    public double avrageFPS = 0;
+
+    public void setAvrageFPS(double avrageFPS) {
+        this.avrageFPS = avrageFPS;
+    }
+
+    public void setWITDH(int WITDH) {
+        this.WITDH = WITDH;
+    }
+
+    public void setHEIGHT(int HEIGHT) {
+        this.HEIGHT = HEIGHT;
+    }
     
 }
