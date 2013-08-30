b0c403cafca791c966c50c70869eab90344700f9
Rune <Rune@Voyager>
2013-08-30 14:27:47 +0200

    Playersprites

--- src/Player/Player.java
+++ src/Player/Player.java
@@ -9,6 +9,10 @@ import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
 
 /**
  *
@@ -20,11 +24,31 @@ public class Player implements KeyListener {
     private int dx, dy;
     private int speed;
     private boolean up,down,left,right;
+    private final int spriteWidth = 16;
+    private final int spriteHeight = 17;
+    private BufferedImage[] walking_sideways;
+    private BufferedImage[] walking_up;
+    private BufferedImage[] idleSprite;
 
     public Player(int x, int y, int speed) {
         this.x = x;
         this.y = y;
         this.speed = speed;
+        
+        try{
+            idleSprite[0] = ImageIO.read(new File("res/bob_sideways_idle.png"));
+            walking_sideways = new BufferedImage[2];
+            walking_up = new BufferedImage[2];
+            BufferedImage img = ImageIO.read(new File("res/bob_sideways.png"));
+            BufferedImage img2 = ImageIO.read(new File("res/bob_up.png"));
+            for (int i = 0; i < walking_up.length; i++) {
+                walking_sideways[i] = img.getSubimage(i*spriteWidth+i, 0, spriteWidth, spriteHeight);
+                walking_up[i] = img2.getSubimage(i*spriteWidth+i, 0, spriteWidth, spriteHeight);
+            }
+            
+        }catch (IOException e){
+            e.printStackTrace();
+        }
     }
 
     public void update() {
@@ -58,22 +82,15 @@ public class Player implements KeyListener {
         int key = e.getKeyCode();
         if (key == KeyEvent.VK_W || key == KeyEvent.VK_UP) {
             up = true;
-            //dy -= 1;
         }
         if (key == KeyEvent.VK_A || key == KeyEvent.VK_LEFT) {
             left = true;
-
-            //dx -= 1;
         }
         if (key == KeyEvent.VK_S || key == KeyEvent.VK_DOWN) {
             down = true;
-
-           // dy += 1;
         }
         if (key == KeyEvent.VK_D || key == KeyEvent.VK_RIGHT) {
             right = true;
-
-            //dx += 1;
         }
     }
 
@@ -82,20 +99,15 @@ public class Player implements KeyListener {
         int key = e.getKeyCode();
         if (key == KeyEvent.VK_W || key == KeyEvent.VK_UP) {
             up = false;
-
-//            dy = y;
         }
         if (key == KeyEvent.VK_A || key == KeyEvent.VK_LEFT) {
             left = false;
-//            dx = x;
         }
         if (key == KeyEvent.VK_S || key == KeyEvent.VK_DOWN) {
             down = false;
-//            dx = x;
         }
         if (key == KeyEvent.VK_D || key == KeyEvent.VK_RIGHT) {
             right = false;
-//            dy = y;
         }
     }
 }
