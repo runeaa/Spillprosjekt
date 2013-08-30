5fa4e7934f3560b36701ccf2a4010448c78e84f2
Rune <Rune@Voyager>
2013-08-30 16:09:23 +0200

    Walking-animation

--- src/Player/Player.java
+++ src/Player/Player.java
@@ -5,6 +5,7 @@
 package Player;
 
 import Main.GamePanel;
+import com.sun.xml.internal.bind.v2.model.core.Adapter;
 import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.event.KeyEvent;
@@ -23,7 +24,7 @@ public class Player implements KeyListener {
     private int x, y;
     private int dx, dy;
     private int speed;
-    private boolean up,down,left,right, facingLeft;
+    private boolean up, down, left, right, facingLeft;
     private final int spriteWidth = 16;
     private final int spriteHeight = 17;
     private BufferedImage[] walking_sideways;
@@ -31,17 +32,14 @@ public class Player implements KeyListener {
     private BufferedImage[] walking_down;
     private BufferedImage[] idleSprite = new BufferedImage[1];
     private Animation animation;
-    private int width;
-    
-    
-    
+
     public Player(int x, int y, int speed) {
         this.x = x;
         this.y = y;
         this.speed = speed;
-        
-        try{
-            
+
+        try {
+
             idleSprite = new BufferedImage[1];
             walking_sideways = new BufferedImage[2];
             walking_up = new BufferedImage[2];
@@ -51,20 +49,23 @@ public class Player implements KeyListener {
             BufferedImage img2 = ImageIO.read(new File("res/bob_up.png"));
             BufferedImage img3 = ImageIO.read(new File("res/bob_down.png"));
             for (int i = 0; i < walking_up.length; i++) {
-                walking_sideways[i] = img.getSubimage(i*spriteWidth, 0, spriteWidth, spriteHeight);
-                walking_up[i] = img2.getSubimage(i*spriteWidth, 0, spriteWidth, spriteHeight);
-                walking_down[i] = img3.getSubimage(i*spriteWidth, 0, spriteWidth, spriteHeight);
+                walking_sideways[i] = img.getSubimage(i * spriteWidth, 0, spriteWidth, spriteHeight);
+                walking_up[i] = img2.getSubimage(i * spriteWidth, 0, spriteWidth, spriteHeight);
+                walking_down[i] = img3.getSubimage(i * spriteWidth, 0, spriteWidth, spriteHeight);
             }
-            
-        }catch (IOException e){
+
+        } catch (IOException e) {
             e.printStackTrace();
         }
-        
+
         animation = new Animation();
         facingLeft = false;
     }
 
     public void update() {
+        System.out.println("DY: " + dy + ", DX: " + dx);
+//        dy = 0;
+//        dx = 0;
         if (up) {
             dy -= speed;
         }
@@ -72,48 +73,47 @@ public class Player implements KeyListener {
             dy += speed;
         }
         if (left) {
-            dx -= speed;
+            dx += speed;
         }
         if (right) {
-            dx += speed;
+            dx -= speed;
         }
         x = dx;
         y = dy;
-        
+
         //sprite animation
-        if(left || right){
+        if (left || right) {
             animation.setFrames(walking_sideways);
-            animation.setDelay(100);
-        }else{
+            animation.setDelay(200);
+        } else {
             animation.setFrames(idleSprite);
             animation.setDelay(-1);
         }
-        
-        if(up){
+
+        if (up) {
             animation.setFrames(walking_up);
-            animation.setDelay(100);
+            animation.setDelay(1000);
         }
-        if(down){
+        if (down) {
             animation.setFrames(walking_down);
-            animation.setDelay(100);
+            animation.setDelay(500);
         }
         animation.update();
-        facingLeft = (dx < 0) ? true : false;
-        
+
     }
 
     public void draw(Graphics2D g) {
         //filler verdier, bruk tikeMap.getX/Y for å gi størrelsen til mappet
         int tx = 50;
         int ty = 50;
-        
-        if(facingLeft){
-            g.drawImage(animation.getImage(), (int) (tx - x - spriteWidth / 2), (int) (ty + y - spriteWidth /2), null);
-        }else{
-            g.drawImage(animation.getImage(),(tx - x - spriteWidth / 2 + spriteWidth), (int) (ty + y - spriteHeight /2),
+
+        if (facingLeft) {
+            g.drawImage(animation.getImage(), (int) (tx - x - spriteWidth / 2), (int) (ty + y - spriteWidth / 2), null);
+        } else {
+            g.drawImage(animation.getImage(), (tx - x - spriteWidth / 2 + spriteWidth), (int) (ty + y - spriteHeight / 2),
                     -spriteWidth, spriteHeight, null);
         }
-        
+
     }
 
     @Override
@@ -127,12 +127,15 @@ public class Player implements KeyListener {
             up = true;
         }
         if (key == KeyEvent.VK_A || key == KeyEvent.VK_LEFT) {
+            facingLeft = false;
             left = true;
         }
         if (key == KeyEvent.VK_S || key == KeyEvent.VK_DOWN) {
             down = true;
         }
         if (key == KeyEvent.VK_D || key == KeyEvent.VK_RIGHT) {
+            facingLeft = true;
+
             right = true;
         }
     }
