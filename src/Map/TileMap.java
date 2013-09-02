26b1b26b93517db52d0a180d570403bf6a7d5eee
Rune <Rune@Voyager>
2013-09-02 10:55:25 +0200

    Txt fil med spørsmål.

---
+++ src/Map/TileMap.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Map;
+
+/**
+ *
+ * @author Rune
+ */
+public class TileMap {
+    
+    
+}
