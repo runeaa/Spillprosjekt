a6c951aa3b3ce4c16e7db77843749ac722998da9
haavamoa <haavamoa@gmail.com>
2013-09-03 16:33:54 +0200

    fikset litt i forhold til dialogbox

--- src/Settings/PlayerSettings.java
+++ src/Settings/PlayerSettings.java
@@ -10,6 +10,10 @@ public class PlayerSettings {
     public String system;
     public String playerName;
     public String devMethod;
+    public static  final int waterfall = 0;
+    public static final int spiral = 1;
+    public static final int up = 2;
+    public static final int scrum = 3;
     public int difficulity = -1; //-1 = no diff choosen.
     public static final int EASY = 0;
     public static final int NORMAL = 1;
@@ -41,5 +45,24 @@ public class PlayerSettings {
 
     public void setDifficulity(int difficulity) {
         this.difficulity = difficulity;
+    }
+    
+    /*
+     *  model.addElement("SCRUM");
+        model.addElement("Fossefallsmetoden");
+        model.addElement("Spiralmetoden");
+        model.addElement("Unified Processing");
+     */
+    public int getDevMethod(String devmethod){
+        if(devmethod.equalsIgnoreCase("SCRUM")){
+            return scrum;
+        }else if(devmethod.equalsIgnoreCase("Unified Processing")){
+            return up;
+        }else if(devmethod.equalsIgnoreCase("Fossefallsmetoden")){
+            return waterfall;
+        }else if(devmethod.equalsIgnoreCase("Spiralmetoden")){
+            return spiral;
+        }
+        return -1;
     }
 }
