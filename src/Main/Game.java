ffb0bd92231f8ed167d6552ce8a1443bcae23df6
Marius <marius@ubuntu.ubuntu-domain>
2013-09-03 15:24:30 +0200

    marius tester

--- src/Main/Game.java
+++ src/Main/Game.java
@@ -13,7 +13,7 @@ import javax.swing.JFrame;
 public class Game {
     public static void main(String[] args) {
         JFrame window = new JFrame("A Programmer's Tale - You will be taught");
-        window.setContentPane(new GameMenu(window));
+        window.setContentPane(new GamePanel());
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setResizable(false);
         window.pack();
