b50e44361a33c07976d467ee249131350286c377
espen <espen@158.38.191.73>
2013-09-04 11:35:42 +0200

    Oval.java, lager textboble og skriver i den

--- src/GameElements/Oval.java
+++ src/GameElements/Oval.java
@@ -7,18 +7,36 @@ package GameElements;
 import java.awt.Color;
 import java.awt.Graphics2D;
 import Player.NPC;
+import java.util.ArrayList;
+
 /**
  *
  * @author espen
  */
 public class Oval {
 
-/*    public void draw(Graphics2D g, NPC npc, Text text) {
-        int x = text.getText().length() / ;
-        
+    private NPC npc;
+    private Text text;
+    private int lines;
+    private ArrayList<String> s;
+
+    public Oval(NPC npc, Text text) {
+        this.npc = npc;
+        this.text = text;
+    }
+
+    public void draw(Graphics2D g) {
+        s = text.getString();
+        lines = text.getLines();
         g.setColor(Color.WHITE);
-        g.fillOval(Integer.parseInt(s[1]) - 10, Integer.parseInt(s[2]) - 60, 150, 40);
+        g.fillOval(npc.getX() - 10, npc.getY() - 60, 300, 40 * lines);
         g.setColor(Color.BLACK);
-        g.drawString(s[0], Integer.parseInt(s[1]), Integer.parseInt(s[2]) - 37);
-    }*/
+        int x = npc.getX();
+        int y = npc.getY();
+        String string = "";
+        for (int i = 0; i < lines; i++) {
+            string = s.get(i);
+            g.drawString(string, x, y += g.getFontMetrics().getHeight());
+        }
+    }
 }
--- src/GameElements/Text.java
+++ src/GameElements/Text.java
@@ -10,27 +10,42 @@ import java.util.ArrayList;
  *
  * @author espen
  */
-class Textt {
+public class Text {
     private String text;
     private ArrayList <String> tabS = new ArrayList<>();
-    public Textt(String text){
+    //private String s = "";
+    private final int stringLength = 25;
+    ArrayList<String> s = new ArrayList<>();
+    public Text(String text){
         this.text  = text;
     }
     public String getText(){
         return text;
     }
+    
+    public int getLines(){
+        return s.size();
+    }
+    
     public void makeTab(){
         int counter = 0;
+        String string = "";
         String[] parts = text.split(" ");
         
         for(int i = 0;i<parts.length;i++){
-            
+            if(counter + parts[i].length() > stringLength ){
+                s.add(string);
+                counter = 0;
+                string = "";
+            }
+            counter += parts[i].length();
+            string += parts[i] + " ";
         }
+        s.add(string);
         
     }
     
-    public void drawString(){
-        
+    public ArrayList<String> getString(){
+        return s;
     }
-    
 }
