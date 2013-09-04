255d48b3404939e305e6976b7efd3a50cd760f07
haavamoa <haavamoa@gmail.com>
2013-09-04 15:51:05 +0200

    added siste filer

---
+++ src/GameElements/FeedbackBox.java
@@ -0,0 +1,34 @@
+package GameElements;
+
+import Quiz.Answer;
+import java.awt.Graphics2D;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+
+/**
+ *
+ * @author haavamoa
+ */
+public class FeedbackBox extends JLabel {
+    private Answer answer;
+    private ImageIcon feedbackImage;
+    
+
+    public FeedbackBox(Answer answer) {
+        this.answer = answer;
+    }
+
+    public void paintComponent(Graphics2D g) {
+        
+        if(answer.isIsCorrect()){
+            feedbackImage = new ImageIcon("./res/img/riktigsvar.png");
+           
+        }else{
+            feedbackImage = new ImageIcon("./res/img/feilsvar.png");
+            
+        }
+        feedbackImage.paintIcon(null, g,220, 120);
+        super.paintComponent(g);
+    }
+}
+    
