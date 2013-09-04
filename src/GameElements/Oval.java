2007408374d016e235340e28e76a2f41f6456578
Rune <r.aasvestad@live.com>
2013-09-04 13:07:58 +0200

    jau

--- src/GameElements/Oval.java
+++ src/GameElements/Oval.java
@@ -7,6 +7,10 @@ package GameElements;
 import java.awt.Color;
 import java.awt.Graphics2D;
 import Player.NPC;
+<<<<<<< HEAD
+=======
+import java.util.ArrayList;
+>>>>>>> branch 'master' of https://github.com/runeaa/Spillprosjekt.git
 
 /**
  *
@@ -14,12 +18,44 @@ import Player.NPC;
  */
 public class Oval {
 
+<<<<<<< HEAD
     /*    public void draw(Graphics2D g, NPC npc, Text text) {
      int x = text.getText().length() / ;
+=======
+    private NPC npc;
+    private Text text;
+    private int lines;
+    private ArrayList<String> s;
+
+    public Oval(NPC npc, Text text) {
+        this.npc = npc;
+        this.text = text;
+        text.makeTab();
+    }
+
+    public void draw(Graphics2D g) {
+>>>>>>> branch 'master' of https://github.com/runeaa/Spillprosjekt.git
         
+<<<<<<< HEAD
      g.setColor(Color.WHITE);
      g.fillOval(Integer.parseInt(s[1]) - 10, Integer.parseInt(s[2]) - 60, 150, 40);
      g.setColor(Color.BLACK);
      g.drawString(s[0], Integer.parseInt(s[1]), Integer.parseInt(s[2]) - 37);
      }*/
+=======
+        s = text.getString();
+        lines = text.getLines();
+        System.out.println(lines);
+        g.setColor(Color.WHITE);
+        g.fillOval(npc.getX() - 10, npc.getY() - 60, 275, 40 * lines);
+        g.setColor(Color.BLACK);
+        int x = npc.getX()+10;
+        int y = npc.getY()-35;
+        String string = "";
+        for (int i = 0; i < lines; i++) {
+            string = s.get(i);
+            g.drawString(string, x, y += g.getFontMetrics().getHeight());
+        }
+    }
+>>>>>>> branch 'master' of https://github.com/runeaa/Spillprosjekt.git
 }
--- src/GameElements/Text.java
+++ src/GameElements/Text.java
@@ -5,17 +5,26 @@
 package GameElements;
 
 import java.util.ArrayList;
+import javax.annotation.PostConstruct;
 
 /**
  *
  * @author espen
  */
-class Textt {
+public class Text {
 
     private String text;
     private ArrayList<String> tabS = new ArrayList<>();
+<<<<<<< HEAD
 
     public Textt(String text) {
+=======
+    //private String s = "";
+    private final int stringLength = 25;
+    ArrayList<String> s = new ArrayList<>();
+
+    public Text(String text) {
+>>>>>>> branch 'master' of https://github.com/runeaa/Spillprosjekt.git
         this.text = text;
     }
 
@@ -23,15 +32,42 @@ class Textt {
         return text;
     }
 
+<<<<<<< HEAD
+=======
+    public int getLines() {
+        return s.size();
+    }
+
+>>>>>>> branch 'master' of https://github.com/runeaa/Spillprosjekt.git
     public void makeTab() {
         int counter = 0;
+        String string = "";
         String[] parts = text.split(" ");
 
         for (int i = 0; i < parts.length; i++) {
+<<<<<<< HEAD
+=======
+            if (counter + parts[i].length() > stringLength) {
+                s.add(string);
+                counter = 0;
+                string = "";
+            }
+            counter += parts[i].length();
+            string += parts[i] + " ";
+>>>>>>> branch 'master' of https://github.com/runeaa/Spillprosjekt.git
         }
+<<<<<<< HEAD
 
+=======
+        s.add(string);
+>>>>>>> branch 'master' of https://github.com/runeaa/Spillprosjekt.git
     }
 
+<<<<<<< HEAD
     public void drawString() {
+=======
+    public ArrayList<String> getString() {
+        return s;
+>>>>>>> branch 'master' of https://github.com/runeaa/Spillprosjekt.git
     }
 }
