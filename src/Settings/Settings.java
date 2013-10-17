b3c378f181337d8c8b78a0ee44a6731b17a29e25
amund <amund.porta@gmail.com>
2013-10-17 12:05:58 +0200

    Fixed audio issue with openJDK

--- src/Settings/Settings.java
+++ src/Settings/Settings.java
@@ -8,9 +8,11 @@ import Main.GameMenu;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Random;
+import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
+import javax.sound.sampled.DataLine;
 import javax.swing.JPanel;
 
 /**
@@ -124,14 +126,15 @@ File file  = new File("./res/music/z3fairy.mid");
             songName = "Theme from Jurassic Park";
         } else if (song.contains("jamesbond")) {
             songName = "Theme from James Bond";
-}
+        }
         File file = new File("./res/music/" + song);
         ais = AudioSystem.
                 getAudioInputStream(file);
         }
-        
+        AudioFormat format = ais.getFormat();
+        DataLine.Info info = new DataLine.Info(Clip.class, format);
         // getAudioInputStream() also accepts a File or InputStream
-        
+        clip = (Clip) AudioSystem.getLine(info);
         clip.open(ais);
         clip.loop(Clip.LOOP_CONTINUOUSLY);
         
